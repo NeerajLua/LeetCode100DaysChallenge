6d202fe494e5a8fb479d909ebd5f58998a2da475
Neeraj Mehra <112273260+NeerajLua@users.noreply.github.com>
2023-09-02 15:42:33 +0530

    Create Day5 LCQ_283.java

---
+++ Day5 LCQ_283.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0;           // Initialize 'i' to 0
+        int n = nums.length;  // Calculate the length of the array 'nums'
+        
+        for (int j = 0; j < n; j++) {  // Start a loop from j = 0 to j = (n-1)
+            if (nums[j] != 0) {  // Check if the current element at 'j' is not zero
+                // Swap non-zero element at index 'j' with element at index 'i'
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;  // Increment 'i' to keep track of non-zero elements
+            }
+        }
+    }
+}
