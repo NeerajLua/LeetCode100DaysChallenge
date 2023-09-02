8f87b8a286ee8d77680f2ed0d6f94dfb5f63614b
Neeraj Mehra <112273260+NeerajLua@users.noreply.github.com>
2023-09-02 10:49:05 +0530

    Update Day1 LCQ_27.java

--- Day1 LCQ_27.java
+++ Day1 LCQ_27.java
@@ -1,12 +1,19 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
+        // Ek variable 'k' ko 0 ke barabar initialize karo. Yeh naya length ko track karne ke liye hai.
         int k = 0;
+        // 'nums' array ke elements par ek ek karke loop chalaao.
       for (int i = 0; i<nums.length; i++){
+          //Check karo ki current element index 'i' par 'val' se alag hai.
           if(nums[i] != val){
+               // Agar current element 'val' se alag hai, toh usse position 'k' par copy karo.
+                // Isse 'val' ko array se nikal diya jaata hai.
               nums[k] = nums[i];
+               // 'k' ki value ko badhao taaki agli non-'val' element ke liye tayyar ho jaaye.
               k++;
           }
       }
+          // 'k' ab naye modified array ki length ko represent karta hai.
         return k;
     }
 }
