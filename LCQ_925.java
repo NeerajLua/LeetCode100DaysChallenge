c0e66f944f96adb92a4739ae69b45bcb1b674be8
Neeraj Mehra <112273260+NeerajLua@users.noreply.github.com>
2024-07-03 12:22:17 +0530

    Create LCQ_925.java

---
+++ LCQ_925.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isLongPressedName(String name, String typed) {
+       if(name.equals(typed)) return true;
+       int i = 0, j = 0;
+
+        while(j<typed.length()){
+            if(i<name.length() && name.charAt(i)==typed.charAt(j)){
+                i++;
+            }
+            else if(j==0 || typed.charAt(j) != typed.charAt(j-1)){
+                return false;
+            }
+            j++;
+        }
+
+        return i==name.length();
+
+    }
+}
